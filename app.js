c711b9d4acfd02c55de3074cf7814ea64567277e
박 성준 <sjp5789@jinhak.com>
2018-10-23 12:01:21 +0900

    Rest_API_study

---
+++ app.js
@@ -0,0 +1,22 @@
+// app.js
+
+// [LOAD PACKAGES]
+var express     = require('express');
+var app         = express();
+var bodyParser  = require('body-parser');
+var mongoose    = require('mongoose');
+
+// [CONFIGURE APP TO USE bodyParser]
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// [CONFIGURE SERVER PORT]
+var port = process.env.PORT || 8080;
+
+// [CONFIGURE ROUTER]
+var router = require('./routes')(app)
+
+// [RUN SERVER]
+var server = app.listen(port, function(){
+ console.log("Express server has started on port " + port)
+});
