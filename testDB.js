634321a81e51a2a2ca32504c3dcf997c3ea64fe1
박 성준 <sjp5789@jinhak.com>
2018-10-24 18:00:56 +0900

    testdb study

--- testDB.js
+++ testDB.js
@@ -1,9 +1,25 @@
+var sql = require('mssql');
+var result;
+var express = require('express');
 const db=
 {
-"user":"DSS2019",
-"password":"DSS2019",
+"user":"TESTDB",
+"password":"TESTDB",
 "server":"10.1.0.62",
-"database":"DSS2019"
+port:1433, 
+"database":"TESTDB",
+encrypt: false,
  };
 
  module.exports=db;
+
+ sql.connect(db,function(err){
+     sql.close();
+ });
+
+
+ var request =new sql.Request();
+ request.query("select * from UserList",function(err,result){
+    console.log(result);
+    
+ });
